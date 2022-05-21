3ced38bd09836c4bfc241bc76c4e0209f2df793c
Ineffa <89753184+Ineffa@users.noreply.github.com>
2022-05-21 16:53:33 -0400

    improved firefly attribute registration

--- src/main/java/com/ineffa/thewildupgrade/registry/TheWildUpgradeEntities.java
+++ src/main/java/com/ineffa/thewildupgrade/registry/TheWildUpgradeEntities.java
@@ -3,7 +3,6 @@ package com.ineffa.thewildupgrade.registry;
 import com.ineffa.thewildupgrade.TheWildUpgrade;
 import com.ineffa.thewildupgrade.entities.FireflyEntity;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
@@ -18,7 +17,9 @@ import net.minecraft.world.biome.BiomeKeys;
 
 public class TheWildUpgradeEntities {
 
-    public static final EntityType<FireflyEntity> FIREFLY = Registry.register(Registry.ENTITY_TYPE, new Identifier(TheWildUpgrade.MOD_ID, "firefly"), FabricEntityTypeBuilder.createMob().entityFactory(FireflyEntity::new)
+    public static final EntityType<FireflyEntity> FIREFLY = Registry.register(Registry.ENTITY_TYPE, new Identifier(TheWildUpgrade.MOD_ID, "firefly"), FabricEntityTypeBuilder.createMob()
+            .entityFactory(FireflyEntity::new)
+            .defaultAttributes(FireflyEntity::createFireflyAttributes)
             .dimensions(EntityDimensions.fixed(0.3125F, 0.375F))
             .spawnGroup(SpawnGroup.AMBIENT)
             .spawnRestriction(SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING, FireflyEntity::canFireflySpawn)
@@ -26,10 +27,6 @@ public class TheWildUpgradeEntities {
     );
 
     public static void initialize() {
-        // Register attributes
-        FabricDefaultAttributeRegistry.register(FIREFLY, FireflyEntity.createFireflyAttributes());
-
-        // Add to biome spawns
         BiomeModifications.addSpawn(context -> {
             RegistryKey<Biome> biomeKey = context.getBiomeKey();
             return (context.getBiome().hasHighHumidity() && context.getBiome().getTemperature() >= 0.5F) || biomeKey == BiomeKeys.DARK_FOREST || biomeKey == BiomeKeys.LUSH_CAVES || biomeKey == BiomeKeys.RIVER;
