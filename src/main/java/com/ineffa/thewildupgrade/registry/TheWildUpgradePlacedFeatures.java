ad78497f494d7e77e1bae6efa0d662828eb199b8
Ineffa <89753184+Ineffa@users.noreply.github.com>
2022-05-24 17:16:36 -0400

    increased chance for small birches in birch forest

--- src/main/java/com/ineffa/thewildupgrade/registry/TheWildUpgradePlacedFeatures.java
+++ src/main/java/com/ineffa/thewildupgrade/registry/TheWildUpgradePlacedFeatures.java
@@ -15,7 +15,7 @@ public class TheWildUpgradePlacedFeatures {
     public static final RegistryEntry<PlacedFeature> FANCY_BIRCH_CHECKED = register("fancy_birch_checked", TheWildUpgradeConfiguredFeatures.FANCY_BIRCH, PlacedFeatures.wouldSurvive(Blocks.BIRCH_SAPLING));
     public static final RegistryEntry<PlacedFeature> FANCY_BIRCH_WITH_BEES_CHECKED = register("fancy_birch_with_bees_checked", TheWildUpgradeConfiguredFeatures.FANCY_BIRCH_WITH_BEES, PlacedFeatures.wouldSurvive(Blocks.BIRCH_SAPLING));
 
-    public static final RegistryEntry<ConfiguredFeature<RandomFeatureConfig, ?>> BIRCH_FOREST_TREES = TheWildUpgradeConfiguredFeatures.register("birch_forest_trees", Feature.RANDOM_SELECTOR, new RandomFeatureConfig(List.of(new RandomFeatureEntry(TreePlacedFeatures.BIRCH_CHECKED, 0.1F), new RandomFeatureEntry(FANCY_BIRCH_WITH_BEES_CHECKED, 0.0125F)), FANCY_BIRCH_CHECKED));
+    public static final RegistryEntry<ConfiguredFeature<RandomFeatureConfig, ?>> BIRCH_FOREST_TREES = TheWildUpgradeConfiguredFeatures.register("birch_forest_trees", Feature.RANDOM_SELECTOR, new RandomFeatureConfig(List.of(new RandomFeatureEntry(TreePlacedFeatures.BIRCH_CHECKED, 0.15F), new RandomFeatureEntry(FANCY_BIRCH_WITH_BEES_CHECKED, 0.0125F)), FANCY_BIRCH_CHECKED));
     public static final RegistryEntry<PlacedFeature> BIRCH_FOREST_TREES_PLACED = register("birch_forest_trees", BIRCH_FOREST_TREES, VegetationPlacedFeatures.modifiersWithWouldSurvive(PlacedFeatures.createCountExtraModifier(5, 0.1f, 1), Blocks.BIRCH_SAPLING));
 
     private static RegistryEntry<PlacedFeature> register(String name, RegistryEntry<? extends ConfiguredFeature<?, ?>> registryEntry, PlacementModifier... modifiers) {
