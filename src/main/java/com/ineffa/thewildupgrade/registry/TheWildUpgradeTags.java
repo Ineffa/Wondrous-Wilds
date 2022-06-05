cffff5c9d7b0b917a51277ff0b64f202181e04ad
Ineffa <89753184+Ineffa@users.noreply.github.com>
2022-06-05 15:27:52 -0400

    added violet item tag

--- src/main/java/com/ineffa/thewildupgrade/registry/TheWildUpgradeTags.java
+++ src/main/java/com/ineffa/thewildupgrade/registry/TheWildUpgradeTags.java
@@ -2,6 +2,7 @@ package com.ineffa.thewildupgrade.registry;
 
 import com.ineffa.thewildupgrade.TheWildUpgrade;
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -13,7 +14,15 @@ public class TheWildUpgradeTags {
         public static final TagKey<Block> VIOLETS = createBlockTag("violets");
     }
 
+    public static class ItemTags {
+        public static final TagKey<Item> VIOLETS = createItemTag("violets");
+    }
+
     private static TagKey<Block> createBlockTag(String name) {
         return TagKey.of(Registry.BLOCK_KEY, new Identifier(TheWildUpgrade.MOD_ID, name));
+    }
+
+    private static TagKey<Item> createItemTag(String name) {
+        return TagKey.of(Registry.ITEM_KEY, new Identifier(TheWildUpgrade.MOD_ID, name));
     }
 }
