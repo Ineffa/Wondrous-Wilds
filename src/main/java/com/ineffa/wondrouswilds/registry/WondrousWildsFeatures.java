b3f129d4969832e3909a0e4f8faf7c56aced1b9a
Ineffa <89753184+Ineffa@users.noreply.github.com>
2022-08-15 07:30:19 -0400

    adjusted birch forest tree chances

--- src/main/java/com/ineffa/wondrouswilds/registry/WondrousWildsFeatures.java
+++ src/main/java/com/ineffa/wondrouswilds/registry/WondrousWildsFeatures.java
@@ -173,10 +173,10 @@ public class WondrousWildsFeatures {
         public static final RegistryEntry<PlacedFeature> TALL_BIRCH_PLACED = registerPlaced("tall_birch", TALL_BIRCH_CONFIGURED, PlacedFeatures.wouldSurvive(Blocks.BIRCH_SAPLING));
 
         public static final RegistryEntry<ConfiguredFeature<RandomFeatureConfig, ?>> BIRCH_FOREST_TREES_CONFIGURED = registerConfigured("birch_forest_trees", Feature.RANDOM_SELECTOR, new RandomFeatureConfig(List.of(
-                new RandomFeatureEntry(TreePlacedFeatures.BIRCH_CHECKED, 0.15F),
-                new RandomFeatureEntry(FANCY_BIRCH_WITH_BEES_PLACED, 0.015F),
-                new RandomFeatureEntry(FANCY_BIRCH_WITH_WOODPECKERS_PLACED, 0.005F)
-        ), FANCY_BIRCH_PLACED));
+                new RandomFeatureEntry(FANCY_BIRCH_PLACED, 0.85F),
+                new RandomFeatureEntry(FANCY_BIRCH_WITH_BEES_PLACED, 0.075F),
+                new RandomFeatureEntry(FANCY_BIRCH_WITH_WOODPECKERS_PLACED, 0.05F)
+        ), TreePlacedFeatures.BIRCH_CHECKED));
         public static final RegistryEntry<PlacedFeature> BIRCH_FOREST_TREES_PLACED = registerPlaced("birch_forest_trees", BIRCH_FOREST_TREES_CONFIGURED, VegetationPlacedFeatures.modifiersWithWouldSurvive(PlacedFeatures.createCountExtraModifier(5, 0.1F, 1), Blocks.BIRCH_SAPLING));
 
         public static final RegistryEntry<ConfiguredFeature<RandomFeatureConfig, ?>> OLD_GROWTH_BIRCH_FOREST_TREES_CONFIGURED = registerConfigured("old_growth_birch_forest_trees", Feature.RANDOM_SELECTOR, new RandomFeatureConfig(List.of(
