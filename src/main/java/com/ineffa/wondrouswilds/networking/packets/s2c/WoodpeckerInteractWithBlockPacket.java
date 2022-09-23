44d3bda2faf29281f842868e78c7ffc797e01baa
Ineffa <89753184+Ineffa@users.noreply.github.com>
2022-09-23 04:40:43 -0400

    Fixed woodpecker interacting crashing on servers

--- src/main/java/com/ineffa/wondrouswilds/networking/packets/s2c/WoodpeckerInteractWithBlockPacket.java
+++ src/main/java/com/ineffa/wondrouswilds/networking/packets/s2c/WoodpeckerInteractWithBlockPacket.java
@@ -9,13 +9,13 @@ import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.world.World;
 
 public final class WoodpeckerInteractWithBlockPacket {
 
@@ -23,24 +23,24 @@ public final class WoodpeckerInteractWithBlockPacket {
 
     @Environment(value = EnvType.CLIENT)
     public static void receive(MinecraftClient client, ClientPlayNetworkHandler handler, PacketByteBuf buf, PacketSender responseSender) {
-        ClientWorld world = client.world;
-        if (world == null) return;
-
         int entityId = buf.readVarInt();
-        Entity entity = client.world.getEntityById(entityId);
-        if (!(entity instanceof WoodpeckerEntity woodpecker)) return;
-
         BlockHitResult hitResult = buf.readBlockHitResult();
         boolean useItem = buf.readBoolean();
 
+        World world = (World) client.world;
+        if (world == null) return;
+
         client.execute(() -> {
+            Entity entity = world.getEntityById(entityId);
+            if (!(entity instanceof WoodpeckerEntity woodpecker)) return;
+
             WoodpeckerFakePlayer fakePlayer = new WoodpeckerFakePlayer(woodpecker);
 
-            if (useItem) world.getBlockState(hitResult.getBlockPos()).onUse(world, fakePlayer, Hand.MAIN_HAND, hitResult);
-            else {
+            if (useItem) {
                 ItemStack heldItem = woodpecker.getMainHandStack();
                 if (!heldItem.isEmpty()) heldItem.useOnBlock(new WoodpeckerItemUsageContext(woodpecker, fakePlayer, heldItem, hitResult));
             }
+            else world.getBlockState(hitResult.getBlockPos()).onUse(world, fakePlayer, Hand.MAIN_HAND, hitResult);
         });
     }
 }
