7240489443c18bc719616919320a4b4946c4ff43
Ineffa <89753184+Ineffa@users.noreply.github.com>
2022-09-28 02:27:35 -0400

    Added Mod Menu integration

---
+++ src/main/java/com/ineffa/wondrouswilds/client/WondrousWildsModMenu.java
@@ -0,0 +1,9 @@
+package com.ineffa.wondrouswilds.client;
+
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(value = EnvType.CLIENT)
+public class WondrousWildsModMenu implements ModMenuApi {
+}
