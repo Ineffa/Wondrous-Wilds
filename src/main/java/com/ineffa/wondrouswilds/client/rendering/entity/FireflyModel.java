c7046e91caf56fbb51e8cc3072c7eef00f935787
Ineffa <89753184+Ineffa@users.noreply.github.com>
2022-10-28 17:49:15 -0400

    Updated to 1.19.2 and fixed GeckoLib crash

--- src/main/java/com/ineffa/wondrouswilds/client/rendering/entity/FireflyModel.java
+++ src/main/java/com/ineffa/wondrouswilds/client/rendering/entity/FireflyModel.java
@@ -6,9 +6,9 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.MathHelper;
 import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.molang.MolangParser;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 import software.bernie.geckolib3.resource.GeckoLibCache;
-import software.bernie.shadowed.eliotlash.molang.MolangParser;
 
 public class FireflyModel extends AnimatedGeoModel<FireflyEntity> {
     private static final Identifier TEXTURE = new Identifier(WondrousWilds.MOD_ID, "textures/entity/firefly/firefly.png");
@@ -40,7 +40,7 @@ public class FireflyModel extends AnimatedGeoModel<FireflyEntity> {
         float limbSwing = fireflyEntity.limbAngle - fireflyEntity.limbDistance * (1.0F - delta);
         float limbSwingAmount = MathHelper.lerp(delta, fireflyEntity.lastLimbDistance, fireflyEntity.limbDistance);
 
-        parser.setValue("query.limb_swing", limbSwing * 1.25D);
-        parser.setValue("query.limb_swing_amount", limbSwingAmount + (0.9F * MathHelper.clamp(limbSwingAmount * 10.0F, 0.0F, 1.0F)));
+        parser.setValue("query.limb_swing", () -> limbSwing * 1.25D);
+        parser.setValue("query.limb_swing_amount", () -> limbSwingAmount + (0.9F * MathHelper.clamp(limbSwingAmount * 10.0F, 0.0F, 1.0F)));
     }
 }
