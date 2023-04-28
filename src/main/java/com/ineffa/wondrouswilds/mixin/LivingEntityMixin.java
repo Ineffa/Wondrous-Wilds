4b3630399f48ca95630ef5907fb5e93efca18c24
Ineffa <89753184+Ineffa@users.noreply.github.com>
2023-04-28 07:11:51 -0400

    Bodkin arrow ignores a percentage of armor on hit

--- src/main/java/com/ineffa/wondrouswilds/mixin/LivingEntityMixin.java
+++ src/main/java/com/ineffa/wondrouswilds/mixin/LivingEntityMixin.java
@@ -4,11 +4,14 @@ import com.ineffa.wondrouswilds.entities.HoppingMob;
 import com.ineffa.wondrouswilds.entities.projectiles.BodkinArrowEntity;
 import com.ineffa.wondrouswilds.registry.WondrousWildsEntities;
 import net.minecraft.entity.*;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -17,6 +20,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
+
+    @Shadow public abstract int getArmor();
+
+    @Shadow public abstract double getAttributeValue(EntityAttribute attribute);
 
     private LivingEntityMixin(EntityType<?> type, World world) {
         super(type, world);
@@ -43,5 +50,11 @@ public abstract class LivingEntityMixin extends Entity {
         float newAmount = amount;
         if (source.getSource() != null && source.getSource().getType() == WondrousWildsEntities.BODKIN_ARROW) newAmount *= 3.0F;
         instance.damageArmor(source, newAmount);
+    }
+
+    @Inject(method = "applyArmorToDamage", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/DamageUtil;getDamageLeft(FFF)F", ordinal = 0, shift = At.Shift.AFTER), cancellable = true)
+    private void bodkinArrowBypassArmor(DamageSource source, float amount, CallbackInfoReturnable<Float> callback) {
+        if (source.getSource() != null && source.getSource().getType() == WondrousWildsEntities.BODKIN_ARROW)
+            callback.setReturnValue(DamageUtil.getDamageLeft(amount, this.getArmor() * 0.75F, (float) this.getAttributeValue(EntityAttributes.GENERIC_ARMOR_TOUGHNESS)));
     }
 }
